eb728640a40bb6b64be2e946102af58a976e3d15
osmanarslan07 <osmanarslan07@yahoo.com>
2023-01-30 21:38:44 +0300

    day8 : ReadingProperties

--- src/test/java/com/cydeo/tests/day08_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/LearningProperties.java
@@ -6,7 +6,11 @@ public class LearningProperties {
 
     @Test
     public void java_properties_reading_test(){
+        //key = value
+        //os.name = Windows 10
+        //user.name = osman
 
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
     }
 }
