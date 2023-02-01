3b047a77b3fb05cec50c4f34f5fbaa303185bf36
osmanarslan07 <osmanarslan07@yahoo.com>
2023-02-01 23:14:06 +0300

    day9 : Driver utilities

--- src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_Config_Practice.java
@@ -1,57 +1,46 @@
 package com.cydeo.tests.day08_properties_config_reader;
 
 import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class T4_Config_Practice {
 
-    WebDriver driver;
-    @BeforeMethod
-    public void setupMethod(){
-
-        String browserType = ConfigurationReader.getProperty("browser");
-
-        //1. Create new test and make set ups
-        driver = WebDriverFactory.getDriver(browserType);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://google.com");
-
-
-    }
+//    public WebDriver driver;
+//
+//    @BeforeMethod
+//    public void setupMethod(){
+//
+//        //We are getting the browserType dynamically from our configuration.properties file
+//        String browserType = ConfigurationReader.getProperty("browser");
+//        driver = WebDriverFactory.getDriver(browserType);
+//
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//    }
 
     @Test
     public void google_search_test(){
+        //Driver.getDriver() --> driver
+
+        Driver.getDriver().get("https://www.google.com");
+
         //3- Write “apple” in search box
-        WebElement googleSearchBox= driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
 
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
 
         //4- Verify title:
         //Expected: apple - Google Search
-        String expectedTitle = ConfigurationReader.getProperty("searchValue") + " - Google Search";
-        String actualTitle = driver.getTitle();
+        String expectedTitle = ConfigurationReader.getProperty("searchValue")+" - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
 
-        Assert.assertEquals(actualTitle,expectedTitle);
-
-
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
-
-
-    //Use `configuration.properties` for the following:
-    //1. The browser type
-    //2. The URL
-    //3. The search keyword
-    //4. Make title verification dynamic. If search value changes, title
-    //assertion should not fail.
 
 }
--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,25 +1,15 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.CRM_Utilities;
-import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class T3_CRM_LOGIN extends TestBase {
 
-public class T3_CRM_LOGIN {
 
-    WebDriver driver;
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
     //TC #3: Login scenario
     @Test
     public void crm_login_test(){
@@ -54,7 +44,7 @@ public class T3_CRM_LOGIN {
 
         //6. Verify title is as expected:
         //EXpected : Portal
-        BrowserUtils.verifyTitle(driver,"Portal");
+        BrowserUtils.verifyTitle(driver,"(1) Portal");
 
     }
 
@@ -69,7 +59,7 @@ public class T3_CRM_LOGIN {
 
         //6. Verify title is as expected:
         //EXpected : Portal
-        BrowserUtils.verifyTitle(driver,"Portal");
+        BrowserUtils.verifyTitle(driver,"(1) Portal");
 
     }
 
